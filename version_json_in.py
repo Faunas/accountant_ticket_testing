c77747897464eb2130645c933b4c62d8817ee9c6
cleo <themax2012@mail.ru>
2023-10-19 22:33:44 +0300

    Добавлен режим "Спрашивать только выученные счета" выученные счета в отдельном jsone. Добавлен вывод в конце каждого опроса, показывающий кол-во правильных и неправильных ответов, а также выводит счета, которые были написаны с ошибкой.
    Исправлен режим рандома, где могли повторяться счета или он мог быть бесконечным.

--- version_json_in.py
+++ version_json_in.py
@@ -13,22 +13,40 @@ def all_numerations(json_data):
         print(f"{i}. {name_check}")
 
 
+def game_end(player_score, json_data, array_with_fail_answer):
+    print(
+        f"----------\nИгра окончена.\nПравильно отвеченные счета: {player_score}\nНеверно отвеченные счета: {json_data - player_score}\n")
+    for i in array_with_fail_answer:
+        print(i)
+    print("----------")
+
 def start_gamemode_random(json_data):
-    while True:
+    question_passed = 1
+    player_score = 0
+    all_data_old = len(json_data)
+    array_with_fail_answer = []
+    while json_data:
         random_key = random.choice(list(json_data.keys()))
         correct_answer = json_data[random_key]
+
 
         user_answer = input(f"Какой счёт относится к {random_key}?\nВаш ответ: ")
 
         if user_answer == correct_answer:
-            print("Верно!")
+            print(f"Верно!\n{question_passed} / {all_data_old}")
+            player_score += 1
         else:
-            print(f"Неверно. Правильный ответ: {correct_answer}")
+            print(f"Неверно. Правильный ответ: {correct_answer}\n{question_passed} / {all_data_old}")
+            array_with_fail_answer.append(correct_answer)
+        json_data.pop(random_key, None)
+        question_passed += 1
+    game_end(player_score, all_data_old, array_with_fail_answer)
 
 
 def start_gamemode_increase(json_data):
     counter_keys_in_json = len(json_data)
     player_score = 0
+    array_with_fail_answer = []
     print(f"Всего будет {counter_keys_in_json} вопроса.\nОпрос будет проводиться с 01 по 99 счёт.")
     for i in json_data:
         correct_answer = json_data[i]
@@ -38,13 +56,14 @@ def start_gamemode_increase(json_data):
             player_score += 1
         else:
             print(f"Неверно. Правильный ответ: {correct_answer}")
-    print(
-        f"Игра окончена.\nПравильно отвеченные счета: {player_score}\nНеверно отвеченные счета: {len(json_data) - player_score}")
+            array_with_fail_answer.append(correct_answer)
+    game_end(player_score, counter_keys_in_json, array_with_fail_answer)
 
 
 def start_gamemode_decrease(json_data):
     counter_keys_in_json = len(json_data)
     player_score = 0
+    array_with_fail_answer = []
     print(f"Всего будет {counter_keys_in_json} вопроса.\nОпрос будет проводиться с 99 по 01 счёта.")
 
     while json_data:
@@ -56,12 +75,13 @@ def start_gamemode_decrease(json_data):
             player_score += 1
         else:
             print(f"Неверно. Правильный ответ: {correct_answer}")
-
-    print(
-        f"Игра окончена.\nПравильно отвеченные счета: {player_score}\nНеверно отвеченные счета: {counter_keys_in_json - player_score}")
+            array_with_fail_answer.append(correct_answer)
+    game_end(player_score, counter_keys_in_json, array_with_fail_answer)
 
 
 def main_menu():
+
+
     json_data_string = '''
     {
       "01": "Основные средства",
@@ -128,14 +148,40 @@ def main_menu():
       "99": "Прибыли и убытки"
     }
     '''
+
+    json_iknow = '''
+    {
+      "01": "Основные средства",
+      "02": "Амортизация основных средств",
+      "03": "Доходные вложения в материальные ценности",
+      "04": "Нематериальные активы",
+      "05": "Амортизация нематериальных активов",
+      "10": "Материалы",
+      "11": "Животные на выращивании и откорме",
+      "20": "Основное производство",
+      "41": "Товары",
+      "50": "Касса",
+      "51": "Расчетные счета",
+      "52": "Валютные счета",
+      "55": "Специальные счета в банках",
+      "80": "Уставный капитал",
+      "90": "Продажи"
+    }
+    '''
     json_data = get_elements_from_string(json_data_string)
+    json_iknow_strf = get_elements_from_string(json_iknow)
 
     print("\nТЕСТ НА ЗНАНИЕ БУХГАЛТЕРСКИХ СЧЕТОВ\n\nНеобходимо выбрать режим подготовки:\n"
           "[1] Вывести список всех счетов\n"
           "[2] Начать подготовку с рандомными счетами\n"
           "[3] Начать подготовку с счетами по возрастанию\n"
-          "[4] Начать подготовку с счетами по убыванию\n")
-    choose = int(input("Введите цифру для выбора соответствующего пункта: "))
+          "[4] Начать подготовку с счетами по убыванию\n"
+          "[5] Повторить точно выученные счета\n")
+    try:
+        choose = int(input("Введите цифру для выбора соответствующего пункта: "))
+    except Exception:
+        print("Введены некорректные символы!")
+        main_menu()
     if choose == 1:
         all_numerations(json_data)
     elif choose == 2:
@@ -144,9 +190,10 @@ def main_menu():
         start_gamemode_increase(json_data)
     elif choose == 4:
         start_gamemode_decrease(json_data)
+    elif choose == 5:
+        start_gamemode_random(json_iknow_strf)
     else:
         print("Введена неизвестная цифра! Повторяю информацию...")
-    print("\n---------ВЫ ВВЕЛИ НЕКОРРЕКТНЫЕ ДАННЫЕ!---------")
 
 
 if __name__ == "__main__":
